db6a1aabc5df6996defde3f93d819ee65cd1758b
ECECSRO <ECECSRO@ES-00000107.ericsson.se>
2018-12-27 00:56:44 +0100

    Improvement 1 - Concurrent chat manager

--- Enunciado Webchat/enunciadowebchat/src/test/java/es/sidelab/webchat/ChatManagerTest.java
+++ Enunciado Webchat/enunciadowebchat/src/test/java/es/sidelab/webchat/ChatManagerTest.java
@@ -2,7 +2,14 @@ package es.sidelab.webchat;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -30,7 +37,7 @@ public class ChatManagerTest {
 		});
 
 		// Crear un nuevo chat en el chatManager
-		chatManager.newChat("Chat2", 5, TimeUnit.SECONDS);
+		chatManager.newChat("Chat", 5, TimeUnit.SECONDS);
 
 		// Comprobar que el chat recibido en el método 'newChat' se llama 'Chat'
 		assertTrue("The method 'newChat' should be invoked with 'Chat', but the value is "
@@ -64,5 +71,83 @@ public class ChatManagerTest {
 		assertTrue("Notified new user '" + newUser[0] + "' is not equal than user name 'user2'",
 				"user2".equals(newUser[0]));
 
+	}
+
+	// Crear el chat Manager
+	ChatManager chatManager = new ChatManager(50);
+
+	@Test
+	public void newConcurrentUserInChat() throws InterruptedException, TimeoutException {
+
+		int concurrentUsers = 4;
+		
+		ExecutorService executor = Executors.newFixedThreadPool(concurrentUsers);
+
+	    ArrayList<Future<String>> futures = new ArrayList<Future<String>>(concurrentUsers);
+		try {
+
+			CompletionService<String> completionService = new ExecutorCompletionService<>(
+					executor);
+
+			for (int i = 0; i < concurrentUsers; i++) {
+				int userId = i;
+	            futures.add(completionService.submit(() -> userTestBehaviour1(userId)));
+			}
+
+			for (int i = 0; i < concurrentUsers; i++) {
+
+				Future<String> completedTask = completionService.take();
+
+				System.out.println("Task: " + completedTask.get());
+			}
+
+		} 
+		catch (ExecutionException ex) {
+			System.out.println("!!!!!!! ERROR !!!!!!! " + ex.getMessage());
+			ex.printStackTrace();
+			assertTrue("Exception: "+ ex.getMessage() + " raised", false);
+	         for (Future<String> f : futures)
+	             f.cancel(true);			
+		}
+		finally {
+			executor.shutdown();
+		}
+	}
+
+	public String userTestBehaviour1(int userId) {
+
+		final int N_ACTIONS = 5;
+		
+//		try {
+//			Thread.sleep((long) (Math.random() * 5000));
+//		} catch (InterruptedException e) {
+//			return " !!!! ERROR !!!!! - Action with " + userId + " interrupted";
+//		}
+
+		try {
+			
+			TestUser user = new TestUser("user"+userId);
+
+			chatManager.newUser(user);
+		
+			Chat chat = null;
+			TestUser userInChatTestUser;
+
+			for (int i = 0; i < N_ACTIONS; i++) {
+				chat = chatManager.newChat("Chat"+i, 5, TimeUnit.SECONDS);
+				chat.addUser(user);
+				for (User userInChat : chat.getUsers()) {
+					userInChatTestUser = (TestUser) userInChat;
+					System.out.println("Action: "+ userId + " --- User: " + userInChatTestUser.getName() + " in chat: " + chat.getName());
+				}
+
+			}
+			return "!!! GREAT !!! - Action with " + userId + " OK";
+		
+		} catch (InterruptedException | TimeoutException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+			return " !!!! ERROR !!!!! - Action with " + userId + " interrupted: ";
+		}
 	}
 }
